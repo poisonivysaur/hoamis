98bce4696a75f584174758043f626e83f2f9e91c
nandarionndr1 <leebet16@gmail.com>
2017-11-07 04:59:41 +0800

    partially fixing DB integration

--- src/java/model/dao/User.java
+++ src/java/model/dao/User.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DAO;
+package model.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,74 +12,81 @@ import java.sql.*;
 import java.util.ArrayList;
 
 import java.util.*;
+import model.Document;
+import model.MovingOutClearance;
+import model.Occupation;
+import model.TransactionReference;
+import model.UserType;
 
 /**
  *
  * A <b>User</b> object contains the different attributes of a user and its functions on the system.
  * this is a DAO object of a User.
  * 
- * <b>IMPORTANT NOTE:</b> this program still does not have the final database object for groups.
+ * PATCH NOTES:
+ * - 10/30/2017: ADDED FB USER
+ * - 11/07/2017: Used USERS from hoamis DB
  * 
  * @author Leebet-PC
- * @version 1.0 
+ * @version 1.02 
  * @since 10/30/2017
+ * @update 11/07/2017
  */
 public class User {
+    
 
-    /**
-     *
-     */
-    protected int user_id;
+    
+    private String fName;
+    private String lName;
+    private String mName;
+    private String userID;
+    private String passwd;
+    private UserType usertype;
+    private java.sql.Date bDate;
+    private Document photo;
+    private Occupation occupation;
+    private java.sql.Date movingIn;
+    private MovingOutClearance movingOutclear;
+    private TransactionReference trx;
 
-    /**
-     *
-     * @return
-     */
-    public int getUser_id() {
-        return user_id;
+    public User(String fName, String lName, String mName, String userID, String passwd, UserType usertype, java.sql.Date bDate, Document photo, Occupation occupation, java.sql.Date movingIn, MovingOutClearance movingOutclear, TransactionReference trx) {
+        this.fName = fName;
+        this.lName = lName;
+        this.mName = mName;
+        this.userID = userID;
+        this.passwd = passwd;
+        this.usertype = usertype;
+        this.bDate = bDate;
+        this.photo = photo;
+        this.occupation = occupation;
+        this.movingIn = movingIn;
+        this.movingOutclear = movingOutclear;
+        this.trx = trx;
+    }
+    public String getfName() {
+        return fName;
+    }
+   
+    public String getlName() {
+        return lName;
+    }
+   
+    public String getUserID() {
+        return userID;
     }
 
-    /**
-     *
-     * @param user_id
-     */
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
+    public String getPasswd() {
+        return passwd;
     }
-
-    /**
-     *
-     * @return
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     *
-     * @param username
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     *
-     */
-    protected String username;
-          
-    /**
-     * Returns user identification number given username
-     * @param username 
-     * @return
-     */
-    public static int getUserID(String username){
+    
+    /*
+    public static int getUserID(String userID){
         int res =-1;
         try{
             Class.forName("com.mysql.jdbc.Driver"); 
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/TestForumDB","root","root"); 
-            PreparedStatement st = con.prepareStatement("select * from user where username=?"); 
-            st.setString(1, username);
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hoamis","root","root"); 
+            PreparedStatement st = con.prepareStatement("select * from USERS where userID=?"); 
+            st.setString(1, userID);
 
             ResultSet rs = st.executeQuery();
              if(rs.next()){
@@ -91,6 +98,7 @@ public class User {
         }
         return res;
     }
+    */
 
     /**
      * Returns all the users of the database
@@ -100,9 +108,9 @@ public class User {
         ResultSet as= null;
         try{
             Class.forName("com.mysql.jdbc.Driver"); 
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/TestForumDB","root","root"); 
-            PreparedStatement st = con.prepareStatement("select * from TestForumDB.user where username != ?"); 
-            st.setString(1,this.username);
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hoamis","root","root"); 
+            PreparedStatement st = con.prepareStatement("select * from hoamis.USERS where userID != ?"); 
+            st.setString(1,this.getUserID());
             ResultSet rs = st.executeQuery();
              
             return rs;
@@ -123,8 +131,8 @@ public class User {
          
          try{
            Class.forName("com.mysql.jdbc.Driver"); 
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/TestForumDB","root","root"); 
-            PreparedStatement st = con.prepareStatement("INSERT INTO TestForumDB.membership(user_user_id, group_group_id,membership_type) VALUES (?,?,1);"); 
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hoamis","root","root"); 
+            PreparedStatement st = con.prepareStatement("INSERT INTO hoamis.USERGROUPS(user_user_id, group_group_id,membership_type) VALUES (?,?,1);"); 
             st.setInt(1, user_id);
             st.setInt(2, group_id);
        
@@ -134,7 +142,8 @@ public class User {
         }
          return false;
      }
-    
+    /*
+    **THIS IS A DUMMY REGISTER ACCOUNT**
      public static boolean pazucc(String username, String password, int usertype){
         try{
             Class.forName("com.mysql.jdbc.Driver"); 
@@ -154,12 +163,32 @@ public class User {
         }
         return false;
     }
-     public static boolean fbLog(String username){
+    */
+    /**
+     * Creating a new account for Facebook enabled login
+     * @param fName
+     * @param lName
+     * @return nothing
+     * this.fName = fName;
+        this.lName = lName;
+        this.mName = mName;
+        this.userID = userID;
+        this.passwd = passwd;
+        this.usertype = usertype;
+        this.bDate = bDate;
+        this.photo = photo;
+        this.occupation = occupation;
+        this.movingIn = movingIn;
+        this.movingOutclear = movingOutclear;
+        this.trx = trx;
+     */
+     public static boolean fbLog(String fName, String lName){
         try{
             Class.forName("com.mysql.jdbc.Driver"); 
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/TestForumDB","root","root"); 
-            PreparedStatement st = con.prepareStatement("INSERT INTO user(username, password, usertype) VALUES ( ?, '1', 1);"); 
-            st.setString(1, username);
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hoamis","root","root"); 
+            PreparedStatement st = con.prepareStatement("INSERT INTO user(fName, lName, userID,passwd,usertype,bDate,photo,occupation,movingIn,movingOutclearID,trxID)"
+                    + " VALUES ( ?, '1', 1);"); 
+            st.setString(1, fName);
 
            int i = st.executeUpdate();
             System.out.println(i);
@@ -170,6 +199,5 @@ public class User {
         }
         return false;
     } 
-    
     
 }
