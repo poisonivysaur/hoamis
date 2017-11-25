b58aef10ea560ea29e25b6659cb8ba320ebdc514
patrishacabigas <31922256+patrishacabigas@users.noreply.github.com>
2017-11-25 16:49:05 +0800

    Add files via upload

---
+++ src/java/model/Vehicle.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Patrisha
+ */
+public class Vehicle {
+    private String plate;
+    private String model;
+    private String make;
+    private String year;
+    private int banned;
+    
+    public Vehicle(){
+        
+    }
+    public Vehicle(String plate, String model, String make, String year, int banned){
+        this.plate = plate;
+        this.model = model;
+        this.make = make;
+        this.year = year + "-0" + "-0";
+        this.banned = banned;
+    }
+    public String getPlate(){
+        return this.plate;
+    }
+    public String getModel(){
+        return this.model;
+    }
+    public String getMake(){
+        return this.make;
+    }
+    public String getYear(){
+        return this.year;
+    }
+    public int getBanned(){
+        return this.banned;
+    }
+    public void setBanned(int banned){
+        this.banned = banned;
+    }
+}
